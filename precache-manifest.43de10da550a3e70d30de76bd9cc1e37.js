e770899446a972951c6846b5faafbdc27ea8a33e
thuy tien le <thuytienle@thuys-MacBook-Air.local>
2020-08-11 16:58:37 -0700

    Updates

---
+++ precache-manifest.43de10da550a3e70d30de76bd9cc1e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547e61b42412e40a69db60b43bf30617",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "066fb0c6fcf674a91bf1",
+    "url": "/covid19/static/css/main.ded22e9a.chunk.css"
+  },
+  {
+    "revision": "efd31248870a6e2bfdd5",
+    "url": "/covid19/static/js/2.7100f12c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.7100f12c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066fb0c6fcf674a91bf1",
+    "url": "/covid19/static/js/main.ab9d7ba4.chunk.js"
+  },
+  {
+    "revision": "636bdfdab0a46f674c1f",
+    "url": "/covid19/static/js/runtime-main.09a13a41.js"
+  }
+]);
